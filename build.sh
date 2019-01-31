17137a183c64bdffa564986d3373ea5d8188882b
任贵杰 <812022339@qq.com>
2019-01-31 13:34:27 +0800

    删掉错误cd..

--- build.sh
+++ build.sh
@@ -6,7 +6,6 @@ GIT_REVISION=`git log -1 --pretty=format:"%h"`
 IMAGE_NAME=registry.cn-beijing.aliyuncs.com/usm/${MODULE}:${TIME}_${GIT_REVISION}
 
 docker build -t ${IMAGE_NAME} .
-cd ..
 
 #docker push ${IMAGE_NAME}
 
