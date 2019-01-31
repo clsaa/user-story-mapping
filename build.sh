02fb031f172985764f2ba2d415c0d72fb8f7a247
任贵杰 <812022339@qq.com>
2019-01-31 12:56:27 +0800

    build脚本错误

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ MODULE=$1
 TIME=`date "+%Y%m%d%H%M"`
 GIT_REVISION=`git log -1 --pretty=format:"%h"`
 IMAGE_NAME=registry.cn-beijing.aliyuncs.com/usm/${MODULE}:${TIME}_${GIT_REVISION}
-cd ${MODULE}
 
 docker build -t ${IMAGE_NAME} .
 cd ..
